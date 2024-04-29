6b4a5da53065fb9b5f84e48c0b368e617979ac9a
tarekyehya <tarek_500@yahoo.com>
2024-04-30 00:46:53 +0300

    Project Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repository contains code and resources for a Kaggle competition focused on training a model to score student essays automatically. The goal is to reduce grading time and costs, enabling essays to be included in testing, a crucial measure of student learning often avoided due to grading challenges.',
+    author='Tarek Yahia',
+    license='MIT',
+)
