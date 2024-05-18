b6e92b2a7b0fea2df41d24faf7549c5b5bdab59f
tarekyehya <tarek_500@yahoo.com>
2024-05-19 02:04:15 +0300

    update config

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
+    RAW_PATH: str
     DF_PATH: str
     TRAIN_PATH: str
     TEST_PATH: str
