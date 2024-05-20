2b4cecb46fd5d591d4091e6ed90b33bcfad9be75
tarekyehya <tarek_500@yahoo.com>
2024-05-20 19:03:02 +0300

    refactor dataPreprocessing class

--- src/controllers/dataPreprocessing.py
+++ src/controllers/dataPreprocessing.py
@@ -17,16 +17,18 @@ class dataPreprocessing:
         test = pl.read_csv(self.raw_path + "train.csv").with_columns(columns)
         return train, test
 
-    def remove_HTML(self,x):
+    @staticmethod
+    def remove_HTML(x):
         html=re.compile(r'<.*?>')
         return html.sub(r'',x) 
     
-    def data_clean(self,x):
+    @staticmethod
+    def data_clean(x):
         # Convert words to lowercase
         x = x.lower()
     
         # Remove HTML
-        x = self.remove_HTML(x)
+        x = dataPreprocessing.remove_HTML(x)
 
         # Delete strings starting with @
         x = re.sub("@\w+", '',x)
@@ -49,9 +51,11 @@ class dataPreprocessing:
         x = x.strip()
         return x
     
-    def full_text_preprocess(self,df):
-        return df.with_columns(pl.col('full_text').map_elements(self.data_clean,return_dtype=str))
+    @staticmethod
+    def full_text_preprocess(df):
+        return df.with_columns(pl.col('full_text').map_elements(dataPreprocessing.data_clean,return_dtype=str))
     
-    def paragraph_preprocess(self,df):
-        return df.with_columns(pl.col('paragraph').map_elements(self.data_clean,return_dtype=str))
+    @staticmethod
+    def paragraph_preprocess(df):
+        return df.with_columns(pl.col('paragraph').map_elements(dataPreprocessing.data_clean,return_dtype=str))
 
